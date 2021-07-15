7db0a4e1b7aff316a0faa7b93efd0ca92b8c6810
horair1990 <87250003+horair1990@users.noreply.github.com>
2021-07-14 20:40:13 -0600

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Horair
+ */
+public class Ejercicio2 {
+    public static void main(String [] args){
+        int n1 = 1, n2 = 2, n3 = 3 , n4 = 4;
+        int suma,resta,multiplicacion;
+        double division;
+        
+        suma = n1+n2;
+        resta = n3-n4;
+        multiplicacion = n4*n2;
+        division = n4 / n1;
+        
+        System.out.println("La suma de " + n1 +" + "+n2 +" es: "+suma);
+        System.out.println("La resta de " + n3 +" - "+n4 +" es: "+resta);
+        System.out.println("La multiplicacion de " + n4 +" * "+n2 +" es: "+multiplicacion);
+        System.out.println("La division de " + n4 +" / "+n1 +" es: "+division);
+       
+    }
+}
+
---
+++ Ejercicio3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Horair
+ */
+public class Ejercicio3 {
+     public static void main(String [] args){
+       int M = 6, T = 1, K = -10 ;
+        boolean p1 = (M>T);
+        boolean p2 = (T/K == -5);
+        boolean p3 = (M+T == 7) || (M-T == 5);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+    }
+}
